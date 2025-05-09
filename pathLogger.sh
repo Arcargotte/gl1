5b8bfec20afc6ff55a237850b84add120c3032cd
Arcargotte <alan.argotte69@gmail.com>
2025-05-09 14:53:05 -0400

    minor changes

--- pathLogger.sh
+++ pathLogger.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-#INITIATE
-#VERIFY IF LOGGER FILE ALREADY EXISTS.
-#IF SO, STARTS EXECUTION
-#OTHERWISE, PROMPT USER FOR FILE NAME AND STARTS EXECUTION
-#IN THE PROGRAM, GIVE USER THE FOLLOWING OPTIONS
-# (1) PRINT STORED REFERENCES
-# (2) ADD NEW REFERENCE
-# (3) ERASE REFERENCE
-# (4) CHANGE ACCESS CONTROL 
-# (5) END PROGRAM
+# Creates a reference file refcontrol at /home/$USER to store
+# an alias for path to a file. 
+
+# (1) Checks if file exists. If exists, proceeds to program.
+# Otherwise created refcontrol.
 
 # BY DEFAULT, FILE NAME IS refcontrol AT /home/$USER
 
@@ -18,6 +13,15 @@ if ! find /home/$USER -type f -name "refcontrol" | grep -q .; then
     echo "refcontrol file has been created at /home/$USER"
     echo "Check the contents of the file to read any references"
 fi
+
+#Through usage of getopts, script works as a bash command.
+#With a flag as an input, proceeds to case for program to work.
+#Flags:
+# -h: prints usage of command
+# -p: prints stored references 
+# -a [REFERENCE] [PATHNAME]: adds a single non-existing reference to pathname into refcontrol 
+# -e [REFERENCE]: deletes reference in refcontrol
+# -c [REFERENCE] [CHMOD ARGUMENT]: changes access control of reference.
 
 while getopts "hpa:r:c:" o; do
     reference=
@@ -37,17 +41,16 @@ while getopts "hpa:r:c:" o; do
             #CHECK IF FILE EXISTS
             #CHECK IF FILE IS A DEVICE
             #CREATE REFERENCE
-
             reference=$(awk '{ print $1 }' <<< $OPTARG)
-            echo "$reference"
             refPath=$(awk '{ print $2 }' <<< $OPTARG)
-            echo "$refPath"
 
-            if grep -q "$reference" /home/$USER/refcontrol; then
+            if grep -q "$reference" /home/$USER/refcontrol || grep -q "$refPath" /home/$USER/refcontrol; then
                 echo "Reference already exists. Aborting!"
                 exit
             fi
 
+            #Checks if pathname exists in refcontrol. The command grep -q . checks if finds returns
+            #a nonempty reference
             if find "$refPath" | grep -q .; then
                 echo "Checking file..."
                 if grep -q "/dev/" <<< "$refPath"; then
@@ -99,8 +102,11 @@ while getopts "hpa:r:c:" o; do
             echo
             echo "-p    prints all references"
             echo "-a    add a new reference"
+            echo "Using -a to add the pair [REFERENCE] [PATH] must be enclosed in double quotes"
             echo "-r    remove a reference"
             echo "-c    change access control of reference"
+            echo "Using -c to add the pair [REFERENCE] [OCTAL-MODE CODE] must be enclosed in double quotes"
+
 
         ;;
     esac
